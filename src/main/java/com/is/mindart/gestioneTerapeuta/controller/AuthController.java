fafa714b55cb7c3d3550ebc1a3192ba85678417b
Emanuele A. <161335121+NumberZeroo@users.noreply.github.com>
2024-12-06 16:30:29 +0100

    fix: import

--- src/main/java/com/is/mindart/gestioneTerapeuta/controller/AuthController.java
+++ src/main/java/com/is/mindart/gestioneTerapeuta/controller/AuthController.java
@@ -5,10 +5,13 @@ import com.is.mindart.gestioneTerapeuta.service.TerapeutaService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
 
 @RestController
 @RequestMapping("/api/terapeuta")

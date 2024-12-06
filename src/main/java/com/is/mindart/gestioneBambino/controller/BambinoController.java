8b5d2211e4c73eddd726cc21c9bbc5160fc82343
gabriele_ristallo <ristallogabriele@gmail.com>
2024-12-06 23:06:18 +0100

    feat(backend): creata la sezione di backend che si occupa di fornire le api per la visualizzazione della lista di tutti i bambini e di un singolo bambino tramite l'id. (Rimane ingestita e si prevede di gestirla in futuro, la visualizzazione dei soli bambini del terapeuta che ha acceduto da realizzare contestualmente al controllo degli accessi). Le classi che sono state create sono: - BambinoController.java - BambinoDTO.java - BambinoRepository.java - BambinoService.java

---
+++ src/main/java/com/is/mindart/gestioneBambino/controller/BambinoController.java
@@ -0,0 +1,54 @@
+package com.is.mindart.gestioneBambino.controller;
+
+import com.is.mindart.gestioneBambino.service.BambinoDTO;
+import com.is.mindart.gestioneBambino.service.BambinoService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/bambino")
+@CrossOrigin(origins = "http://localhost:3000")
+public class BambinoController {
+
+    /**
+     * Servizio per la gestione dei bambini.
+     */
+    @Autowired
+    private BambinoService bambinoService;
+
+    /**
+     * @autor gabrieleristallo
+     * Restituisce tutti i bambini presenti nel database.
+     *
+     * @return lista di bambini presenti nel database
+     */
+    @GetMapping("/getall")
+    public ResponseEntity<List<BambinoDTO>> getAllBambini() {
+        List<BambinoDTO> bambini = bambinoService.getAllBambini();
+        return ResponseEntity.ok(bambini);
+    }
+
+    /**
+     * @author gabrieleristallo
+     * Restituisce il bambino con l'identificativo specificato.
+     *
+     * @param id identificativo del bambino
+     * @return bambino con l'identificativo specificato
+     */
+    @GetMapping("/get/{id}")
+    public ResponseEntity<BambinoDTO> getBambino(@PathVariable final Long id) {
+        BambinoDTO bambino = bambinoService.getBambino(id);
+        return ResponseEntity.ok(bambino);
+    }
+
+//    @PostMapping("/add")
+//    public ResponseEntity<BambinoDTO> addBambino(
+//            @Valid @RequestBody final BambinoDTO bambinoDto) {
+//        bambinoService.addBambino(bambinoDto);
+//        return ResponseEntity.ok(bambinoDto);
+//    }
+}

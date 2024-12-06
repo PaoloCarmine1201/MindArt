c2b3dbf24b4d0d5f8282caccfccaa8ea19f834fc
Maurilio La Rocca <maurilio.larocca@gmail.com>
2024-12-06 23:24:12 +0100

    feat(frontend): added frontend validation to bambino registration form

--- src/frontend/src/components/RegisterBambino/RegisterBambino.js
+++ src/frontend/src/components/RegisterBambino/RegisterBambino.js
@@ -1,25 +1,18 @@
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import {useState} from "react";
-import {Form} from "react-bootstrap";
+import RegisterBambinoValidatedForm from "./RegisterBambinoValidatedForm";
 
 function RegisterBambino(){
 
     const [show, setShow] = useState(false);
-    const [inputs, setInputs] = useState({});
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-    const handleChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setInputs(values => ({...values, [name]: value}))
-    }
-
-    function handleSubmit(){
-        //TODO: implementare la chiamata al backend
-        alert(inputs);
+    function handleSubmit(values) {
+        console.log('Submitted values:', values);
+        setShow(false);
     }
 
     return (
@@ -29,7 +22,6 @@ function RegisterBambino(){
                    onHide={handleClose}
                    backdrop="static"
                    keyboard={false}
-                   size="lg"
                    aria-labelledby="contained-modal-title-vcenter"
                    centered
             >
@@ -38,48 +30,12 @@ function RegisterBambino(){
                 </Modal.Header>
 
                 <Modal.Body>
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group controlId="registerBambino.nome">
-                            <Form.Label>Nome:</Form.Label>
-                            <Form.Control type="text" placeholder="Es. peppino"/>
-                        </Form.Group>
-
-                        <Form.Group controlId="registerBambino.cognome">
-                            <Form.Label>Cognome:</Form.Label>
-                            <Form.Control type="text" placeholder="Es. rossi"/>
-                        </Form.Group>
-                        <Form.Group controlId="registerBambino.sesso">
-                            <Form.Label>Sesso:</Form.Label>
-                            <Form.Select aria-label="Sesso:">
-                                <option value="maschio">Maschio</option>
-                                <option value="femmina">Femmina</option>
-                            </Form.Select>
-                        </Form.Group>
-                        <Form.Group controlId="registerBambino.dataDiNascita">
-                            <Form.Label>Data di nascita:</Form.Label>
-                            <Form.Control type="date" />
-                        </Form.Group>
-
-                        <Form.Group controlId="registerBambino.codiceFiscale">
-                            <Form.Label>Codice fiscale:</Form.Label>
-                            <Form.Control type="text" />
-                        </Form.Group>
-
-                        <Form.Group controlId="registerBambino.emailGenitore">
-                            <Form.Label>Email genitore:</Form.Label>
-                            <Form.Control type="email" />
-                        </Form.Group>
-
-                        <Form.Group controlId="registerBambino.telefonoGenitore">
-                            <Form.Label>Numero telefono genitore:</Form.Label>
-                            <Form.Control type="text" />
-                        </Form.Group>
-                    </Form>
+                    <RegisterBambinoValidatedForm handleSubmit={handleSubmit}></RegisterBambinoValidatedForm>
                 </Modal.Body>
 
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>Annulla</Button>
-                    <Button variant="primary" onClick={handleSubmit}>Conferma</Button>
+                    <Button variant="primary" type="submit" form="registerBambinoForm">Conferma</Button>
                 </Modal.Footer>
             </Modal>
         </>

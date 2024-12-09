7b1e2c49a3e070db70bda592a9d2221f1e9cc2a6
Alessandro Pinto <alessandropinto2003@gmail.com>
2024-12-09 18:51:54 +0100

    feat: RF-T01F added Login Page

--- src/frontend/src/components/button/Button.js
+++
@@ -1,9 +0,0 @@
-const Button = ({ type, label, onClick }) => {
-    return (
-        <button className={`btn btn-${type}`} onClick={onClick}>
-            {label}
-        </button>
-    );
-};
-
-export default Button;

f7bf88f69f354074aadd55d46eff0aa6073882e7
Emanuele19 <emanuele190403@gmail.com>
2024-12-20 17:57:21 +0100

    feat: RF_T24 - Aggiunta materiale

--- src/main/java/com/is/mindart/gestioneMateriale/model/MaterialeRepository.java
+++ src/main/java/com/is/mindart/gestioneMateriale/model/MaterialeRepository.java
@@ -2,5 +2,13 @@ package com.is.mindart.gestioneMateriale.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MaterialeRepository extends JpaRepository<Materiale, Long> {
+    /**
+     * In JPA: select m from Materiale m where m.terapeuta.id = :terapeutaId.
+     * @param terapeutaId - id del terapeuta
+     * @return lista di materiali associati
+     */
+    List<Materiale> findByTerapeutaId(Long terapeutaId);
 }

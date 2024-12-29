01a28a21cd88674ab878d356ef96ee64e0981185
Alessandro Pinto <alessandropinto2003@gmail.com>
2024-12-29 18:43:07 +0100

    fix: Fixed backend, now does not require the session code

--- src/main/java/com/is/mindart/gestioneSessione/controller/SessioneControllerBambino.java
+++ src/main/java/com/is/mindart/gestioneSessione/controller/SessioneControllerBambino.java
@@ -4,6 +4,8 @@ import com.is.mindart.gestioneMateriale.model.Materiale;
 import com.is.mindart.gestioneMateriale.service.MaterialeDTO;
 import com.is.mindart.gestioneMateriale.service.MaterialeService;
 import com.is.mindart.gestioneMateriale.service.OutputMaterialeDTO;
+import com.is.mindart.gestioneSessione.model.Sessione;
+import com.is.mindart.gestioneSessione.model.SessioneRepository;
 import com.is.mindart.gestioneSessione.service.SessioneDTO;
 import com.is.mindart.gestioneSessione.service.SessioneService;
 import lombok.RequiredArgsConstructor;
@@ -37,20 +39,22 @@ public class SessioneControllerBambino {
      */
     private final MaterialeService materialeService;
 
+    /**
+     *  Provvede ad accedere al database per l'entità Sessione.
+     */
+    private final SessioneRepository sessioneRepository;
 
     /**
      *
-     * @param id della sessione
      * @return Materiale far visualizzare il video
      */
-    @GetMapping("/getMateriale/{id}")
-    public ResponseEntity<MaterialeDTO> getMateriale(
-            @PathVariable final long id) {
+    @GetMapping("/getMateriale/")
+    public ResponseEntity<MaterialeDTO> getMateriale() {
         Authentication authentication = SecurityContextHolder
                 .getContext().getAuthentication();
-        String idBambino = (String) authentication.getPrincipal();
-
-        Materiale materiale = sessioneService.getMaterialeFromSessione(id);
+        String codiceBambino = (String) authentication.getPrincipal();
+        Sessione sessione = sessioneRepository.findByTerminataFalseAndBambini_CodiceOrderByDataAsc(codiceBambino).getFirst();
+        Materiale materiale = sessione.getMateriale();
 
         String path = materiale.getPath();
         try {

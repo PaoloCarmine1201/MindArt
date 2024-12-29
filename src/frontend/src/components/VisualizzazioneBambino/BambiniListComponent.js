415c2ec6a626505dd16e80f3d6519671cf810528
Emanuele A. <161335121+NumberZeroo@users.noreply.github.com>
2024-12-29 15:08:26 +0100

    feat: rimozione bambino
    Co-authored-by: Giuseppe0075 <giuseppesica03@gmail.com>

--- src/frontend/src/components/VisualizzazioneBambino/BambiniListComponent.js
+++ src/frontend/src/components/VisualizzazioneBambino/BambiniListComponent.js
@@ -1,33 +1,62 @@
 import { Link } from "react-router-dom";
 import BambinoListItemComponent from "./BambinoListItemComponent";
-import '../../style/BambiniListStyle.css';
-import BambinoListItemButtonComponent from "./BambinoListItemButtonComponent";
+import "../../style/BambiniListStyle.css";
+import { Button } from "react-bootstrap";
+import axiosInstance from "../../config/axiosInstance";
+import React from "react";
 
 function BambiniListComponent(props) {
     console.log(props);
-    const bambini = props.bambini;
-    const button = props.button;
-    const from = props.from;
+    const { bambini, button } = props;
+
     return (
         <>
             <div className="bambini-list-container">
-                <div className="container">
-                    {bambini && bambini.length > 0 ? (
-                        bambini.map((bambino) => (
-                            <Link
-                                key={bambino.id}
-                                to={`/dettaglioBambino/${bambino.id}`}
-                                className="bambino-link"
-                            >
-                                {button ? <BambinoListItemButtonComponent bambino={bambino}/> :
-                                    <BambinoListItemComponent bambino={bambino} />}
-                                <hr className={"separatore"}></hr>
-                            </Link>
-                        ))
-                    ) : (
-                        <p>Nessun bambino trovato</p>
-                    )}
-                </div>
+                {bambini && bambini.length > 0 ? (
+                    bambini.map((bambino) => (
+                        <div key={bambino.id}>
+                            <div style={{ display: "flex", alignItems: "center" }}>
+
+                                {/* Link ai details del bambino */}
+                                <Link
+                                    to={`/dettaglioBambino/${bambino.id}`}
+                                    className="bambino-link"
+                                    style={{ flex: 1, borderRadius: "10px" }}
+                                >
+                                    <BambinoListItemComponent bambino={bambino} />
+                                </Link>
+
+                                {/* Se button è true, allora visualizzo i pulsanti (utilizzato per differenziare tra widget e fullpage */}
+                                {button ? (
+                                    <>
+                                        <Button
+                                            className="btn-conferma"
+                                            style={{ marginLeft: "10px" }}
+                                            onClick={() => {
+                                                axiosInstance
+                                                    .delete(
+                                                        `http://localhost:8080/api/terapeuta/bambino/${bambino.id}`
+                                                    )
+                                                    .then((response) => {
+                                                        console.log(response);
+                                                    })
+                                                    .catch((error) => {
+                                                        console.error(error);
+                                                    });
+                                            }}
+                                        >
+                                            Disegni
+                                        </Button>
+                                    </>
+                                ) : null}
+                            </div>
+
+                            <hr className="separatore" />
+                        </div>
+                    ))
+                ) : (
+                    <p>Nessun bambino trovato</p>
+                )}
             </div>
         </>
     );

93812f30fa4f76c08109530989af5fca6ec04ddc
Giuseppe0075 <giuseppesica03@gmail.com>
2025-01-03 17:21:03 +0100

    fix: Redirect a Login dopo la registrazione

--- src/frontend/src/pages/registration/Registration.js
+++ src/frontend/src/pages/registration/Registration.js
@@ -82,14 +82,8 @@ const Registration = () => {
                 position: 'bottom-right'
             });
 
-            // Optionally, reset form fields
-            setNome('');
-            setCognome('');
-            setEmail('');
-            setPassword('');
-            setTerms(false);
-            setFieldErrors({});
-            setError('');
+            // Redirect to the login page
+            window.location = '/login';
         } catch (err) {
             console.error('Registration error:', err);
 

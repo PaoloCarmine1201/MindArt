8a51726e78f369d78e8af73f231c0d51bae41dac
Giuseppe0075 <giuseppesica03@gmail.com>
2025-01-06 20:19:12 +0100

    fix: Checkstyle per SessioneDTOValidatorTest

--- src/test/java/com/is/mindart/gestioneSessione/service/SessioneDTOValidatorTest.java
+++ src/test/java/com/is/mindart/gestioneSessione/service/SessioneDTOValidatorTest.java
@@ -12,170 +12,252 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+/**
+ * Test class for {@link SessioneDTOValidator}.
+ */
 class SessioneDTOValidatorTest {
 
+    /**
+     * Validator da testare.
+     */
     private SessioneDTOValidator validator;
 
+    /**
+     * Mock del contesto di validazione.
+     */
     @Mock
     private ConstraintValidatorContext constraintValidatorContext;
 
+    /**
+     * Mock del builder per le violazioni di constraint.
+     */
     @Mock
-    private ConstraintValidatorContext.ConstraintViolationBuilder violationBuilder;
+    private ConstraintValidatorContext
+            .ConstraintViolationBuilder violationBuilder;
 
+    /**
+     * Inizializza i mock e configura il comportamento
+     * del {@link ConstraintValidatorContext}.
+     */
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
         validator = new SessioneDTOValidator();
-        when(constraintValidatorContext.buildConstraintViolationWithTemplate(anyString()))
+
+        when(constraintValidatorContext
+                .buildConstraintViolationWithTemplate(anyString()))
                 .thenReturn(violationBuilder);
 
-        // Dato che il ConstraintValidatorContext normalmente viene iniettato dal container di Spring
-        // bisogna configurarne il comportamento
-        ConstraintValidatorContext.ConstraintViolationBuilder.NodeBuilderCustomizableContext nodeBuilder =
-                mock(ConstraintValidatorContext.ConstraintViolationBuilder.NodeBuilderCustomizableContext.class);
-        when(violationBuilder.addPropertyNode(anyString())).thenReturn(nodeBuilder);
-        when(nodeBuilder.addConstraintViolation()).thenReturn(constraintValidatorContext);
+        ConstraintValidatorContext.ConstraintViolationBuilder
+                .NodeBuilderCustomizableContext nodeBuilder =
+                mock(ConstraintValidatorContext.ConstraintViolationBuilder
+                        .NodeBuilderCustomizableContext.class);
+        when(violationBuilder.addPropertyNode(anyString()))
+                .thenReturn(nodeBuilder);
+        when(nodeBuilder.addConstraintViolation())
+                .thenReturn(constraintValidatorContext);
     }
 
-
+    /**
+     * Test per la validazione di una sessione di
+     * tipo DISEGNO con materiale non nullo.
+     * Deve risultare non valida.
+     */
     @Test
-    @DisplayName("Validazione di Tipo DISEGNO con materiale non nullo -> Non valido")
+    @DisplayName("Validazione di Tipo DISEGNO con materiale "
+            + "non nullo -> Non valido")
     void testTipoDisegnoConMateriale() {
-        // Inizializzazione dati di test
         SessioneDTO sessioneDTO = new SessioneDTO();
         sessioneDTO.setTipoSessione(TipoSessione.DISEGNO);
-        sessioneDTO.setMateriale(1L); // ID del materiale (non valido per DISEGNO)
+        // ID del materiale (non valido per DISEGNO)
+        sessioneDTO.setMateriale(1L);
 
-        // Inizio test e assert
-        boolean isValid = validator.isValid(sessioneDTO, constraintValidatorContext);
+        boolean isValid =
+                validator.isValid(sessioneDTO, constraintValidatorContext);
         assertFalse(isValid);
         verify(constraintValidatorContext, times(1))
-                .buildConstraintViolationWithTemplate("Sessione di Tipo DISEGNO non può avere del materiale");
+                .buildConstraintViolationWithTemplate(
+                        "Sessione di Tipo DISEGNO non può avere del materiale");
     }
 
+    /**
+     * Test per la validazione di una sessione di tipo
+     * DISEGNO senza tema assegnato.
+     * Deve risultare non valida.
+     */
     @Test
-    @DisplayName("Validazione di Tipo DISEGNO senza tema assegnato -> Non valido")
+    @DisplayName("Validazione di Tipo DISEGNO senza tema "
+            + "assegnato -> Non valido")
     void testTipoDisegnoSenzaTemaAssegnato() {
-        // Inizializzazione dati di test
         SessioneDTO sessioneDTO = new SessioneDTO();
         sessioneDTO.setTipoSessione(TipoSessione.DISEGNO);
-        sessioneDTO.setMateriale(null); // Nessun materiale
-        sessioneDTO.setTemaAssegnato(null); // Nessun tema assegnato (non valido)
+        // Nessun materiale
+        sessioneDTO.setMateriale(null);
+        // Nessun tema assegnato (non valido)
+        sessioneDTO.setTemaAssegnato(null);
 
-        // Inizio test e assert
-        boolean isValid = validator.isValid(sessioneDTO, constraintValidatorContext);
+        boolean isValid =
+                validator.isValid(sessioneDTO, constraintValidatorContext);
         assertFalse(isValid);
         verify(constraintValidatorContext, times(1))
-                .buildConstraintViolationWithTemplate("Sessione di Tipo DISEGNO deve avere un tema assegnato");
+                .buildConstraintViolationWithTemplate(
+                        "Sessione di Tipo DISEGNO"
+                                + "deve avere un tema assegnato");
     }
 
+    /**
+     * Test per la validazione corretta di una sessione di tipo DISEGNO.
+     * Deve risultare valida.
+     */
     @Test
     @DisplayName("Validazione di Tipo DISEGNO corretta -> Valido")
     void testTipoDisegnoValido() {
-        // Inizializzazione dati di test
         SessioneDTO sessioneDTO = new SessioneDTO();
         sessioneDTO.setTipoSessione(TipoSessione.DISEGNO);
         sessioneDTO.setMateriale(null); // Nessun materiale
         sessioneDTO.setTemaAssegnato("Tema valido");
 
-        // Inizio test e assert
-        boolean isValid = validator.isValid(sessioneDTO, constraintValidatorContext);
+        boolean isValid =
+                validator.isValid(sessioneDTO, constraintValidatorContext);
         assertTrue(isValid);
     }
 
+    /**
+     * Test per la validazione di una sessione di
+     * tipo COLORE con più di un bambino.
+     * Deve risultare non valida.
+     */
     @Test
-    @DisplayName("Validazione di Tipo COLORE con più di un bambino -> Non valido")
+    @DisplayName("Validazione di Tipo COLORE "
+            + "con più di un bambino -> Non valido")
     void testTipoColoreConPiuBambini() {
-        // Inizializzazione dati di test
         SessioneDTO sessioneDTO = new SessioneDTO();
         sessioneDTO.setTipoSessione(TipoSessione.COLORE);
         sessioneDTO.setBambini(List.of(1L, 2L)); // Più di un bambino
         sessioneDTO.setMateriale(1L); // ID del materiale (valido)
 
-        // Inizio test e assert
-        boolean isValid = validator.isValid(sessioneDTO, constraintValidatorContext);
+        boolean isValid =
+                validator.isValid(sessioneDTO, constraintValidatorContext);
         assertFalse(isValid);
         verify(constraintValidatorContext, times(1))
-                .buildConstraintViolationWithTemplate("Sessione di Tipo COLORE deve avere esattamente un bambino");
+                .buildConstraintViolationWithTemplate(
+                        "Sessione di Tipo COLORE"
+                                + " deve avere esattamente un bambino");
     }
 
+    /**
+     * Test per la validazione di una sessione di tipo COLORE senza materiale.
+     * Deve risultare non valida.
+     */
     @Test
     @DisplayName("Validazione di Tipo COLORE senza materiale -> Non valido")
     void testTipoColoreSenzaMateriale() {
-        // Inizializzazione dati di test
         SessioneDTO sessioneDTO = new SessioneDTO();
         sessioneDTO.setTipoSessione(TipoSessione.COLORE);
         sessioneDTO.setBambini(List.of(1L)); // Un bambino
         sessioneDTO.setMateriale(null); // Nessun materiale (non valido)
 
-        // Inizio test e assert
-        boolean isValid = validator.isValid(sessioneDTO, constraintValidatorContext);
+        boolean isValid =
+                validator.isValid(sessioneDTO, constraintValidatorContext);
         assertFalse(isValid);
         verify(constraintValidatorContext, times(1))
-                .buildConstraintViolationWithTemplate("Sessione di Tipo COLORE deve avere un materiale");
+                .buildConstraintViolationWithTemplate(
+                        "Sessione di Tipo COLORE deve avere un materiale");
     }
 
+    /**
+     * Test per la validazione di una sessione di
+     * tipo COLORE con tema assegnato.
+     * Deve risultare non valida.
+     */
     @Test
     @DisplayName("Validazione di Tipo COLORE con tema assegnato -> Non valido")
     void testTipoColoreConTemaAssegnato() {
-        // Inizializzazione dati di test
         SessioneDTO sessioneDTO = new SessioneDTO();
         sessioneDTO.setTipoSessione(TipoSessione.COLORE);
-        sessioneDTO.setBambini(List.of(1L)); // Un bambino
-        sessioneDTO.setMateriale(1L); // ID del materiale
-        sessioneDTO.setTemaAssegnato("Tema non consentito"); // Tema assegnato (non valido)
+        // Un bambino
+        sessioneDTO.setBambini(List.of(1L));
+        // ID del materiale
+        sessioneDTO.setMateriale(1L);
+        // Tema assegnato (non valido)
+        sessioneDTO.setTemaAssegnato("Tema non consentito");
 
-        // Inizio test e assert
-        boolean isValid = validator.isValid(sessioneDTO, constraintValidatorContext);
+        boolean isValid =
+                validator.isValid(sessioneDTO, constraintValidatorContext);
         assertFalse(isValid);
         verify(constraintValidatorContext, times(1))
-                .buildConstraintViolationWithTemplate("Sessione di Tipo DISEGNO non può avere un tema assegnato");
+                .buildConstraintViolationWithTemplate(
+                        "Sessione di Tipo COLORE non può "
+                                + "avere un tema assegnato");
     }
 
+    /**
+     * Test per la validazione di una sessione di
+     * tipo APPRENDIMENTO senza materiale.
+     * Deve risultare non valida.
+     */
     @Test
-    @DisplayName("Validazione di Tipo APPRENDIMENTO senza materiale -> Non valido")
+    @DisplayName("Validazione di Tipo APPRENDIMENTO "
+            + "senza materiale -> Non valido")
     void testTipoApprendimentoSenzaMateriale() {
-        // Inizializzazione dati di test
         SessioneDTO sessioneDTO = new SessioneDTO();
         sessioneDTO.setTipoSessione(TipoSessione.APPRENDIMENTO);
         sessioneDTO.setMateriale(null); // Nessun materiale (non valido)
 
-        // Inizio test e assert
-        boolean isValid = validator.isValid(sessioneDTO, constraintValidatorContext);
+        boolean isValid =
+                validator.isValid(sessioneDTO, constraintValidatorContext);
         assertFalse(isValid);
         verify(constraintValidatorContext, times(1))
-                .buildConstraintViolationWithTemplate("Sessione di Tipo APPRENDIMENTO deve avere un materiale");
+                .buildConstraintViolationWithTemplate(
+                        "Sessione di Tipo APPRENDIMENTO "
+                                + "deve avere un materiale");
     }
 
+    /**
+     * Test per la validazione di una sessione di
+     * tipo APPRENDIMENTO con tema assegnato.
+     * Deve risultare non valida.
+     */
     @Test
-    @DisplayName("Validazione di Tipo APPRENDIMENTO con tema assegnato -> Non valido")
+    @DisplayName("Validazione di Tipo APPRENDIMENTO "
+            + "con tema assegnato -> Non valido")
     void testTipoApprendimentoConTemaAssegnato() {
-        // Inizializzazione dati di test
         SessioneDTO sessioneDTO = new SessioneDTO();
         sessioneDTO.setTipoSessione(TipoSessione.APPRENDIMENTO);
-        sessioneDTO.setMateriale(1L); // ID del materiale
-        sessioneDTO.setTemaAssegnato("Tema non consentito"); // Tema assegnato (non valido)
+        // ID del materiale
+        sessioneDTO.setMateriale(1L);
+        // Tema assegnato (non valido)
+        sessioneDTO.setTemaAssegnato("Tema non consentito");
 
-        // Inizio test e assert
-        boolean isValid = validator.isValid(sessioneDTO, constraintValidatorContext);
+        boolean isValid =
+                validator.isValid(sessioneDTO, constraintValidatorContext);
         assertFalse(isValid);
         verify(constraintValidatorContext, times(1))
-                .buildConstraintViolationWithTemplate("Sessione di Tipo DISEGNO non può avere un tema assegnato");
+                .buildConstraintViolationWithTemplate(
+                        "Sessione di Tipo APPRENDIMENTO "
+                                + "non può avere un tema assegnato");
     }
 
+    /**
+     * Test per la validazione corretta di una sessione di tipo APPRENDIMENTO.
+     * Deve risultare valida.
+     */
     @Test
     @DisplayName("Validazione di Tipo APPRENDIMENTO corretta -> Valido")
     void testTipoApprendimentoValido() {
-        // Inizializzazione dati di test
         SessioneDTO sessioneDTO = new SessioneDTO();
         sessioneDTO.setTipoSessione(TipoSessione.APPRENDIMENTO);
         sessioneDTO.setMateriale(1L); // ID del materiale
         sessioneDTO.setTemaAssegnato(null); // Nessun tema assegnato
 
-        // Inizio test e assert
-        boolean isValid = validator.isValid(sessioneDTO, constraintValidatorContext);
+        boolean isValid =
+                validator.isValid(sessioneDTO, constraintValidatorContext);
         assertTrue(isValid);
     }
 }
---
+++ src/test/java/com/is/mindart/gestioneSessione/service/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * package-info per package test gestioneSessione.
+ */
+package com.is.mindart.gestioneSessione.service;
